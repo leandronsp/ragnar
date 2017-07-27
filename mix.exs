6a0c6431ffb2c148d60aa4fa0df4022c3fa0a8a2
Leandro Maríngolo <leandronsp@gmail.com>
2017-07-27 06:20:09 +0100

    upgrade to elixir 1.5

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Ragnar.Mixfile do
 
   def project do
     [app: :ragnar,
-     version: "0.0.1",
-     elixir: "~> 1.2",
+     version: "1.0.0",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
